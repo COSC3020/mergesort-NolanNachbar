8c75899228fa924565f921654b2ada0f0dbb9992
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-03 10:38:06 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,30 +1,29 @@
 function mergesort(array) {
     var subarraysize = 1;
 
-    while (subarraysize < array.length) 
-    {
-      for (var start = 0; start < array.length; start += 2 * subarraysize) 
-      {
-        var midsub = Math.min(start + subarraysize, array.length);
-        var endsub = Math.min(start + 2 * subarraysize, array.length);
-        
-        for(var i = start; i <= midsub; i++) 
-        {
-          for(var j = midsub; j <= endsub; j++) 
-          {
-            if(array[i] > array[j])
-            {
-              let t = array[i];
-              array[i] = array[j];
-              array[j] = t;
+    while (subarraysize < array.length) {
+        for (var start = 0; start < array.length; start += 2 * subarraysize) {
+            var midsub = Math.min(start + subarraysize, array.length);
+            var endsub = Math.min(start + 2 * subarraysize, array.length);
+            
+            var a = start, b = midsub;
+            
+            while (a < b && b < endsub) {
+                if (array[a] <= array[b]) {
+                    a++;
+                } else {
+                    let temp = array[b];
+                    for (let k = b; k > a; k--) {
+                        array[k] = array[k - 1];
+                    }
+                    array[a] = temp;
+
+                    a++;
+                    b++;
+                }
             }
-          }
-        
         }
-        
-      
-    }
-      subarraysize *= 2;
+        subarraysize *= 2;
     }
 
     return array;
