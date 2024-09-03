f261df71c9a4dfe5e7b705e11fb0e72483ab744d
NolanNachbar <112431921+NolanNachbar@users.noreply.github.com>
2024-09-02 20:00:48 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,28 @@
 function mergesort(array) {
+    var subarraysize = 1;
+    var subIndex = 0;
+    
+    while (2*subarraysize <= array.length)
+    {
+      while(subIndex*subarraysize <= array.length)
+      {
+      for(var i = subIndex*subarraysize; i <= (subIndex + 1)*subarraysize; i++) 
+      {
+        for(var j = (subIndex + 1)*subarraysize; j <= (subIndex + 2)*subarraysize; j++) 
+      {
+        if(array[i] > array[j])
+        {
+          let t = array[i];
+          array[i] = array[j];
+          array[j] = t;
+        }
+      }
+      }
+      subIndex++;
+      }
+      subIndex = 0;
+      subarraysize *= 2;
+    }
+    
     return array;
 }
